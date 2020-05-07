e10fc69c67c7c09666c5f8388359138ec1b608ca
88-Rising <348514635@qq.com>
2020-05-07 19:15:39 +0800

    springboot入门

---
+++ src/main/java/frank/model/Duck.java
@@ -0,0 +1,17 @@
+package frank.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Duck {
+  private String name;
+
+  private Integer age;
+
+    public Duck(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
