aa63962656e9fb70ec15f2d8612cdd7e64ce289b
88-Rising <348514635@qq.com>
2020-05-08 19:04:22 +0800

    @RequestMapping 重定向和转发的区别

--- src/main/java/frank/controller/LoginController.java
+++ src/main/java/frank/controller/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/user")
+
 public class LoginController {
     @RequestMapping("/login")
     @ResponseBody
@@ -17,5 +19,13 @@ public class LoginController {
     @RequestMapping("/login2")
     public String login2(){
         return "login.html";
+    }
+
+    @RequestMapping("/login3")
+    public String login3(){
+        //重定向发送两次request请求，转发一次request请求
+        //重定向会改变url，转发不会改变url
+        //在tomcat中重定向可以定向到外边的信息，比如百度，但是转发只能访问内部文件
+        return  "redirect:/user/login";
     }
 }
